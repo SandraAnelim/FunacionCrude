826ba6e0fa9db803a2e29aa97104ce11d4df55a2
Nathalia Esquivel Jaramillo <niesquivel@misena.edu.co>
2021-04-07 15:36:30 -0500

    se creó la clase de la capa DAL que conecta con la base de datos (DbContextFundacion).

---
+++ FunacionCrude/Models/DAL/DbContextFundacion.cs
@@ -0,0 +1,20 @@
+﻿using FunacionCrude.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FunacionCrude.Models.DAL
+{
+    public class DbContextFundacion: DbContext
+    {
+        public DbContextFundacion(DbContextOptions <DbContextFundacion> options): base (options)
+        {
+
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Padrino> Padrinos { get; set; }
+
+    }
+}
--- FunacionCrude/Models/Entities/Padrino.cs
+++ FunacionCrude/Models/Entities/Padrino.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -25,6 +26,7 @@ namespace FunacionCrude.Models.Entities
         [Required(ErrorMessage = "Se requiere  UsuarioId")]
         public int UsuarioId { get; set; }
 
+        [Column ("Descripcion", TypeName="nvarchar(250)")]
         public string Descripcion { get; set; }
 
 
