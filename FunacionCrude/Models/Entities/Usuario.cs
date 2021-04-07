0051c6b508bc2f213bb45fb50482facf572fb367
Nathalia Esquivel Jaramillo <niesquivel@misena.edu.co>
2021-04-07 09:39:28 -0500

    se crean las capas DAL, Business, Entities, Abstract y la clase Usuario en la capa Entities

---
+++ FunacionCrude/Models/Entities/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FunacionCrude.Models.Entities
+{
+    public class Usuario
+    {
+
+    }
+}
