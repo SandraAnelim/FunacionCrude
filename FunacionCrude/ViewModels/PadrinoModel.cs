870e63b21a5d66f8f22331c062a4f8b067ab85dd
Anelim Cataño <catano.sandra@gmail.com>
2021-04-11 00:41:21 -0500

    Se crea botones eliminat, detalle, editar, regresar y guardar. Se crea clase view models.

---
+++ FunacionCrude/ViewModels/PadrinoModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FunacionCrude.ViewModels
+{
+    public class PadrinoModel
+    {
+        public int PadrinoId { get; set; }
+
+        public string Nombre { get; set; }
+
+      
+        public string Correo { get; set; }
+
+        
+        public string Contraseña { get; set; }
+
+        public int Edad { get; set; }
+
+        public string Profesion { get; set; }
+                  
+        public int UsuarioId { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
