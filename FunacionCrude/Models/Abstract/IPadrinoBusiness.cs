543c1a2c391211e762aee2f326b1e770e021c61c
Nathalia Esquivel Jaramillo <niesquivel@misena.edu.co>
2021-04-11 23:10:43 -0500

    se crea interfas en la capa abstract.

---
+++ FunacionCrude/Models/Abstract/IPadrinoBusiness.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FunacionCrude.Models.Abstract
+{
+    interface IPadrinoBusiness
+    {
+    }
+}
